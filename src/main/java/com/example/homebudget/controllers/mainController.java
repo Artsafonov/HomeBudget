90d22653128a140843ecc41152cd5a70eeee0166
Artem Safonov <artsafonov19@gmail.com>
2022-02-05 12:53:26 +0100

    Initial commit

---
+++ src/main/java/com/example/homebudget/controllers/mainController.java
@@ -0,0 +1,18 @@
+package com.example.homebudget.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller public class mainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Home page");
+        return "HomePage";
+    }
+
+
+}
